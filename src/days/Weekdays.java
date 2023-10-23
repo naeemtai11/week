ee8dfcc72253f96ac2596a156fae05d3be1002d7
naeemtai11 <naeemtai@sheridancollege.ca>
2023-10-23 15:55:33 -0400

    mid term paper question

--- src/days/Weekdays.java
+++ src/days/Weekdays.java
@@ -1,43 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package days;
 
-/**
- *
- * @author sivagamasrinivasan
- */
-public class Weekdays 
-{
-  public void nameOfDay(String code)
-     {
-    switch(code.toUpperCase())
-      {
-      case "ONE":
-        System.out.println("Monday");
-        break;
-      case "TWO":
-        System.out.println("Tuesday");
-        break;
-      case "THREE":
-        System.out.println("Wednesday");
-        break;
-      case "FOUR":
-        System.out.println("Thursday");
-        break;
-      case "FIVE":
-        System.out.println("Friday");
-        break;
-      case "SIX":
-        System.out.println("Saturday");
-        break;
-      case "SEVEN":
-        System.out.println("Sunday");
-        break;
+public class Weekdays {
+
+    // Enum declaration inside the Weekdays class
+    public enum Day {
+        MONDAY("Monday"),
+        TUESDAY("Tuesday"),
+        WEDNESDAY("Wednesday"),
+        THURSDAY("Thursday"),
+        FRIDAY("Friday"),
+        SATURDAY("Saturday"),
+        SUNDAY("Sunday");
+
+        private final String name;
+
+        Day(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
-  
-}
-}
 
-
+    // Method to print all days
+    public void printAllDays() {
+        for (Day day : Day.values()) {
+            System.out.println(day.getName());
+        }
+    }
+}
